335f43afd2a922e67aeb006aee615705acb10838
dan <dan@coav.ru>
2015-10-20 13:12:22 +0300

    1

--- mo-log/mo-log.js
+++ mo-log/mo-log.js
@@ -1,40 +1,37 @@
-var winston = require('winston');
-require('winston-rollbar');
-
-var trans = [];
-
-trans.push(new (winston.transports.Console)({
-    level: process.env.LOG_LEVEL || 'info'
-}));
+var rollbar = require("rollbar");
 
 var rollbar_token = process.env.ROLLBAR_ACCESS_TOKEN;
 if (rollbar_token)
-    trans.push(new (winston.transports.Rollbar)({
-        rollbarAccessToken: rollbar_token,
-        level: 'warn'
-    }));
+    rollbar.init(rollbar_token);
 
-var logger = new (winston.Logger)({
-    transports: trans,
-    exitOnError: false
-});
+var logger = log.bind('info');
+logger.info = log.bind('info');
+logger.error = log.bind('error');
+logger.warn = log.bind('warn');
+module.exports = logger;
 
-module.exports = {
-    info: logger.info,
-    error: logger.error,
-    verbose: logger.verbose,
-    warn: logger.warn
-};
+function log() {
+    call(console[this], arguments);
+
+    if (this == 'error' || this == 'warn') {
+        rollbar.reportMessageWithPayloadData(arguments[0], {
+            level: this,
+            custom: arguments
+        });
+    }
+}
+
+function call(func, agrs) {
+    func.apply(func, agrs);
+}
 
 // Unhandled exceptions to rollbar:
-
 var handleExceptions = true;
 if (typeof process.env.SUPPRESS_EXCEPTIONS_TO_ROLLBAR !== 'undefined') {
     handleExceptions = !!process.env.SUPPRESS_EXCEPTIONS_TO_ROLLBAR;
 }
 
 if (rollbar_token && handleExceptions) {
-    var rollbar = require("rollbar");
     rollbar.handleUncaughtExceptions(rollbar_token, {
         exitOnUncaughtException: true
     });
